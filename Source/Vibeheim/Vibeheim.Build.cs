032f0f6bb378407e2e252ee3d8ee165cf457f713
grizendi <grizendi@gmail.com>
2025-08-16 14:20:05 -0700

    adds PCG plugins

--- Source/Vibeheim/Vibeheim.Build.cs
+++ Source/Vibeheim/Vibeheim.Build.cs
@@ -8,7 +8,7 @@ public class Vibeheim : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "PCG","PCGGeometryScriptInterop", "GeometryScriptingCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
