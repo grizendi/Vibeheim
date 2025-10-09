3224944234fe682d3b844148899dcd2c3921f016
grizendi <grizendi@gmail.com>
2025-10-09 12:10:28 -0700

    Add engine guard and prewarm PCG assets

--- Source/Vibeheim/Vibeheim.cpp
+++ Source/Vibeheim/Vibeheim.cpp
@@ -3,6 +3,7 @@
 #include "Vibeheim.h"
 #include "Modules/ModuleManager.h"
 
-static_assert(ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION == 6, "Requires UE 5.6.x");
+static_assert(ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION == 6,
+              "Vibeheim requires Unreal Engine 5.6.x");
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Vibeheim, "Vibeheim" );
